38ba60b98a5c78cd5610b089fa366d744a8ff857
obi tobenna <obitobenna6@gmail.com>
2024-01-23 04:36:49 -0800

    assignment on python variables

---
+++ assignment102.py
@@ -0,0 +1,25 @@
+name = "obi emmanuel Tobenna"
+age = 20
+height = 1.75 
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+
+my_variable = "This is a variable." #snake_case
+myVariable = "This is another variable." #camelCase
+MY_CONSTANT = "This is a constant." #ALL_CAPS
+
+print("My name is {}, I am {} years old, and my height is {} metres.".format(name, age, height))
+
+age = age * 2
+
+print("My new age is", age)
+
+print(type(name))
+print(type(age))
+print(type(height))
+
+
+
